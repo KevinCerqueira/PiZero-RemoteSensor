8cb8433f949e8795a9b7e35987be24f54ff7e53e
kevin-credpago <38994372+KevinCerqueira@users.noreply.github.com>
2022-06-23 01:06:11 -0300

    :sparkles: listagem das medições na view

--- src/web/index.php
+++ src/web/index.php
@@ -16,101 +16,96 @@
 	<!-- jquery 3.6.0 -->
 	<script src="vendor/jquery/jquery-3.6.0.min.js"></script>
 
-	
-	<style>
-		.centro {
-			width: 100%;
-			height: 50px;
-			line-height: 50px;
-			text-align: center;
-			background-color: rgba(10, 23, 55, 0.7);
-			color: white;
-
-			/* pura mágica */
-			position: absolute;
-			top: 50%;
-			/* posiciona na metade da tela */
-			margin-top: -25px;
-			/* e retrocede metade da altura */
-			/* margin: 0% 5% 0% 5%; */
-		}
-
-		.p-10 {
-			padding: 10%;
-		}
-	</style>
-
-	<title>Sensores de monitoramento</title>
+	<title>Monitoramento</title>
 </head>
 
-<body>
+<body class="bg-dark">
 
-	<div class="">
-		<div class="centro text-center bg-white p-3 rounded shadow">
-			<div class="row" id="list">
-				<div class="col-md-4 p-0">
-					<div id="high" class="lists m-1 text-white">
-
+	<div class="p-5">
+		<div class="list-group">
+			<a class="list-group-item list-group-item-action active" aria-current="true">
+				<div class="text-center">
+					<h5 class="mb-1">Lista das 10 últimas medições</h5>
+					<small class="text-white">Refresh: #</i><span id="count">0</span></small>
+					<div hidden id="alert-error" class="alert alert-danger mt-3" role="alert">
+						<p id="alert-text-error" class="h5 m-0"></p>
 					</div>
 				</div>
-				<div class="col-md-4 p-0">
-					<div id="medium" class="lists m-1 text-white">
-
-					</div>
+			</a>
+			<div id="medicoes">
+				<div id="loading">
+					<a href="#" class="list-group-item list-group-item-action">
+						<div class="d-flex w-100 justify-content-between">
+							<div class="spinner-grow text-dark" role="status">
+								<span class="visually-hidden">Carregando...</span>
+							</div>
+							<div class="spinner-grow text-dark" role="status">
+								<span class="visually-hidden">Carregando...</span>
+							</div>
+							<div class="spinner-grow text-dark" role="status">
+								<span class="visually-hidden">Carregando...</span>
+							</div>
+							<div class="spinner-grow text-dark" role="status">
+								<span class="visually-hidden">Carregando...</span>
+							</div>
+							<div class="spinner-grow text-dark" role="status">
+								<span class="visually-hidden">Carregando...</span>
+							</div>
+							<div class="spinner-grow text-dark" role="status">
+								<span class="visually-hidden">Carregando...</span>
+							</div>
+						</div>
+					</a>
 				</div>
-				<div class="col-md-4 p-0">
-					<div id="normal" class="lists m-1 text-white">
+				<div class="lists">
 
-					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 
 	<script>
-		// setInterval(requestData, 3000);
-		// async function requestData() {
-		// 	await $.ajax({
-		// 		type: "GET",
-		// 		url: "controllers/measures.php",
-		// 		dataType: "html",
-		// 		beforeSend: function() {
-		// 			$('#alert-error').attr('hidden', '');
-		// 			$('#alert-text-error').text('');
+		$(function(event) {
+					setInterval(requestData, 1000);
+					async function requestData() {
+						await $.ajax({
+							type: "GET",
+							url: "controllers/measures.php",
+							dataType: "html",
+							beforeSend: function() {
+								$('#alert-error').attr('hidden', '');
+								$('#alert-text-error').text('');
 
-		// 			if ($('#count').text() == '0') {
-		// 				$('#loading').removeAttr('hidden');
-		// 			}
-		// 		},
-		// 		success: function(data) {
-		// 			response = JSON.parse(data);
-		// 			if (response.success) {
-		// 				$('.lists').empty();
+								if ($('#count').text() == '0') {
+									$('#loading').removeAttr('hidden');
+								}
+							},
+							success: function(data) {
+								response = JSON.parse(data);
+								if (response.success) {
+									$('.lists').empty();
 
-		// 				$("#high").append(response.data.high);
-		// 				$("#medium").append(response.data.medium);
-		// 				$("#normal").append(response.data.normal);
-		// 				$('#count').text(parseInt($('#count').text()) + 1);
-		// 				if (response.alert) {
-		// 					console.log(sound.play());
-		// 				}
-		// 			} else {
-		// 				$('#alert-error').removeAttr('hidden');
-		// 				$('#alert-text-error').text('Não foi possível carregar a lista: ' + response.error);
-		// 			}
+									$(".lists").append(response.data);
 
-		// 		},
-		// 		error: function(data) {
-		// 			console.log(data);
-		// 			$('#alert-error').removeAttr('hidden');
-		// 			$('#alert-text-error').text('Parece que estamos offline. Chame o TI!');
-		// 		},
-		// 		complete: function() {
-		// 			$('#priorities').removeAttr('hidden');
-		// 			$('#loading').attr('hidden', '');
-		// 		}
-		// 	});
-		// }
+									$('#count').text(parseInt($('#count').text()) + 1);
+								} else {
+									$('#alert-error').removeAttr('hidden');
+									$('#alert-text-error').text('Não foi possível carregar a lista: ' + response.error);
+								}
+
+							},
+							error: function(data) {
+								console.log(data);
+								$('#alert-error').removeAttr('hidden');
+								$('#alert-text-error').text('Parece que estamos offline. Chame o TI!');
+							},
+							complete: function() {
+								$('#priorities').removeAttr('hidden');
+								$('#loading').attr('hidden', '');
+							}
+						});
+					}
+				});
 	</script>
 
 </body>
