790823b7d1a0512c6f6868750c068135a1302f43
kevin-credpago <38994372+KevinCerqueira@users.noreply.github.com>
2022-06-29 15:14:02 -0300

    adicionando declarações

--- src/raspberry/both_v5.c
+++ src/raspberry/both_v5.c
@@ -1,3 +1,17 @@
+/**
+ * Componente Curricular: MI Concorrência e Conectividade
+ * Autor: Esdras Abreu, Guilherme Nobre e Kevin Cerqueira
+ *
+ * Declaro que este código foi elaborado por mim de forma individual e
+ * não contém nenhum trecho de código de outro colega ou de outro autor,
+ * tais como provindos de livros e apostilas, e páginas ou documentos
+ * eletrônicos da Internet. Qualquer trecho de código de outra autoria que
+ * uma citação para o  não a minha está destacado com  autor e a fonte do
+ * código, e estou ciente que estes trechos não serão considerados para fins
+ * de avaliação. Alguns trechos do código podem coincidir com de outros
+ * colegas pois estes foram discutidos em sessões tutorias.
+ */
+
 // compilar: gcc both_v4.c read_voltage.c -o both4 -lwiringPi -lwiringPiDev -lm -lpthread -lmosquitto
 
 #include <stdio.h>
--- src/raspberry/mqtt_pub.c
+++ src/raspberry/mqtt_pub.c
@@ -1,3 +1,17 @@
+/**
+ * Componente Curricular: MI Concorrência e Conectividade
+ * Autor: Esdras Abreu, Guilherme Nobre e Kevin Cerqueira
+ *
+ * Declaro que este código foi elaborado por mim de forma individual e
+ * não contém nenhum trecho de código de outro colega ou de outro autor,
+ * tais como provindos de livros e apostilas, e páginas ou documentos
+ * eletrônicos da Internet. Qualquer trecho de código de outra autoria que
+ * uma citação para o  não a minha está destacado com  autor e a fonte do
+ * código, e estou ciente que estes trechos não serão considerados para fins
+ * de avaliação. Alguns trechos do código podem coincidir com de outros
+ * colegas pois estes foram discutidos em sessões tutorias.
+ */
+
 #include <stdio.h>
 #include <mosquitto.h>
 
